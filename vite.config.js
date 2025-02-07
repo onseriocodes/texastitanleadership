976cf4a36758667fb84aacc81b73722485dd2f2c
Tony <tony.@Mac.lan>
2025-02-07 15:46:55 -0600

    base root added

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-  },
-
-  build: {
-    outDir: "dist", // Customize if needed (default: dist)
-    sourcemap: false, // Turn off for smaller production builds
-    minify: "esbuild", // Ensures optimized output (default setting)
+    base: "/",
   },
 });
 
